887f5b0091fb80cf40751687063f0bafc4779b1c
Rapelang Lepeli <rapelanglepeli2000@gmail.com>
2021-02-23 14:39:02 +0200

    First Commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function menu(){
+    var topnav = document.getElementById("myTopnav");
+    if(topnav.className === "topnav"){
+        topnav.className += " responsive";
+    }else{
+        topnav.className = "topnav";
+    }
+}
